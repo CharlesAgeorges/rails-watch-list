2db43075cd18588dfc46c057fabcf781f7d70aab
Charles AGEORGES <charles.ageorges@gmail.com>
2025-11-28 17:57:39 +0100

    exo

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,6 @@
-//= link_directory ../images
+//= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_directory ../../javascript .js
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
 //= link popper.js
 //= link bootstrap.min.js
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-//= link controllers/index.js
